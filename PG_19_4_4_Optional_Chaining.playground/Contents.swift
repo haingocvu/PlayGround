a50f01264a90c6aa972b409c7bec6144c3fc2479
Hai Vu <hvu20@dxc.com>
2019-04-03 17:04:51 +0700

    optional chanining

---
+++ PG_19_4_4_Optional_Chaining.playground/Contents.swift
@@ -0,0 +1,12 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+//Optional chaining as an Alternative To Forced Unwrapped
+
+//optional chaining tương tự như forced unwrapped.
+//tuy nhiên forced unwrapped gây ra runtime eror nếu optional value = nil
+
+//giá trị của toàn bộ biểu thức sẽ là 1 optional value
+
+
